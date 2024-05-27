8c7a6bdfdfee5a66a96e5f4e26cc24ca95e9d90f
Rafael Jovel <rafael.jovel@mh.gob.sv>
2024-05-26 21:38:50 -0600

    initial commit

---
+++ admin/constante.php
@@ -0,0 +1,7 @@
+<?php
+
+define("BD", "solumaq");
+define("SERVIDOR", "cp9v2o5ds78s73cocbe0-a.oregon-postgres.render.com");
+define("USUARIO", "solumaq");
+define("CLAVE", "HjOK5nLMqAkhH9hEOFq4ADTCVgfyTHbT");
+define("PUERTO", 5432);
